849999c53fd471c97d9472a6662f15e1b719653f
nhdang05 <nguyenhaidang1162005@gmail.com>
2024-11-18 10:12:08 +0700

    Add project files.

---
+++ BAI12_BangCuuChuong/BAI12_BangCuuChuong.cpp
@@ -0,0 +1,16 @@
+﻿// BAI12_BangCuuChuong.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 2; i <= 9; i++) {
+        cout << "bang cuu chuong " << i << ":" << endl;
+        for (int j = 2; j <= 9; j++) {
+            cout << i << " x " << j << " = " << i * j << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
